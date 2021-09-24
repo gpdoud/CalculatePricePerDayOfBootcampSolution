f9950709dab44ce3137f2f44956c28def81cf5f0
Greg Doud <gpdoud@gmail.com>
2021-09-24 09:57:38 -0400

    Add project files.

---
+++ CalculatePricePerDayOfBootcamp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CalculatePricePerDayOfBootcamp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var CostOfBootcamp = 15500m;
+			var NumberOfDays = 65;
+			var PricePerDay = CostOfBootcamp / NumberOfDays;
+			Console.WriteLine($"The boot camp costs {PricePerDay} per day.");
+		}
+	}
+}
